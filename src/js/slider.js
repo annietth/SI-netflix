5f5071efee73d8b45ef2bbd08763a4c15aa19d14
annie.trn <annietth@users.noreply.github.com>
2020-04-10 02:23:00 +0200

    npm run dev worked

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,30 @@
-const gridButton = document.querySelector(".grid");
-const slider = document.querySelector(".slider");
-gridButton.addEventListener("click", () => {
-  slider.classList.remove("slider--list");
-  slider.classList.add("slider--grid");
+//const gridButton = document.querySelector(".grid");
+//const slider = document.querySelector(".slider");
+//gridButton.addEventListener("click", () => {
+//slider.classList.remove("slider--list");
+//slider.classList.add("slider--grid");
+//});
+
+//const listButton = document.querySelector(".list");
+
+//listButton.addEventListener("click", () => {
+//slider.classList.add("slider--list");
+//slider.classList.remove("slider--grid");
+//});
+const heart = document.querySelector(".like");
+
+heart.addEventListener("click", function () {
+  heart.setAttribute("src", "/like-hover.21428334.png");
 });
 
-const listButton = document.querySelector(".list");
+const heart2 = document.querySelector(".like__middle");
 
-listButton.addEventListener("click", () => {
-  slider.classList.add("slider--list");
-  slider.classList.remove("slider--grid");
+heart2.addEventListener("click", function () {
+  heart2.setAttribute("src", "/like-hover.21428334.png");
+});
+
+const heart3 = document.querySelector(".like__left");
+
+heart3.addEventListener("click", function () {
+  heart3.setAttribute("src", "/like-hover.21428334.png");
 });
